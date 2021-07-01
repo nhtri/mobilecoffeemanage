ef88480134ccf8416ea200a22c32e479dd4b7b99
Tri Nguyen <tri.nguyen1@tyme.com>
2021-07-01 13:04:19 +0700

    update

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -339,7 +339,7 @@ export class UserComponent implements OnInit {
         this.image9,
         this.image10,
         this.no,
-        this.giamoi
+        // this.giamoi
       ]
       console.log(this.device)
 
